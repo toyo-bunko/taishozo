515482606c5b5b5f2fc144055ec5754711e6e8a7
Satoru Nakamura <na.kamura.1263@gmail.com>
2024-03-12 12:07:34 +0900

    fix: item

---
+++ components/pages/item/Text.vue
@@ -0,0 +1,187 @@
+<template>
+  <table border="1" style="border-collapse: collapse" width="100%" class="my-2">
+    <tr>
+      <th :colspan="colsKihon.length">
+        <h2>{{ 'テキスト情報' }}</h2>
+      </th>
+    </tr>
+
+    <tr>
+      <th colspan="4">
+        <h3>{{ '勘同目録' }}</h3>
+      </th>
+      <th colspan="4">
+        <h3>{{ '脚注' }}</h3>
+      </th>
+    </tr>
+    <tr>
+      <th width="10%">{{ '底本/校本' }}</th>
+      <th width="10%">{{ '❹' }}</th>
+      <th width="12%">{{ '❼' }}</th>
+      <th width="12%">{{ '❼備考' }}</th>
+      <th width="10%">{{ '底本/校本' }}</th>
+      <th width="10%">{{ '新添部分' }}</th>
+      <th width="12%">{{ 'テキスト' }}</th>
+      <th width="12%">{{ '備考' }}</th>
+    </tr>
+
+    <tr class="text-center">
+      <td>
+        {{ $utils.format(apiResult['ex:勘同目録'][0]['ex:底本/校本']) }}
+      </td>
+      <td class="pl-1 text-left">
+        {{ $utils.format(apiResult['ex:勘同目録'][0]['ex:❹']) }}
+      </td>
+      <td class="pl-1 text-left">
+        {{ $utils.format(apiResult['ex:勘同目録'][0]['ex:❼']) }}
+      </td>
+      <td class="pl-1 text-left">
+        {{ $utils.format(apiResult['ex:勘同目録'][0]['ex:❼備考']) }}
+      </td>
+      <td>
+        {{ $utils.format(apiResult['ex:脚注'][0]['ex:底本/校本']) }}
+      </td>
+      <td class="pl-1 text-left">
+        {{ $utils.format(apiResult['ex:脚注'][0]['ex:新添']) }}
+      </td>
+      <td class="pl-1 text-left">
+        <template
+          v-if="apiResult['ex:酉目'] && apiResult['ex:酉目'].length > 0"
+        >
+          <a :href="apiResult['ex:酉目'][0]['ex:url'][0]" target="_blank"
+            >{{ $utils.format(apiResult['ex:脚注'][0]['ex:テキスト']) }}
+            <v-icon color="primary" small>mdi-exit-to-app</v-icon></a
+          >
+        </template>
+        <template v-else>
+          {{ $utils.format(apiResult['ex:脚注'][0]['ex:テキスト']) }}
+        </template>
+
+        <template v-if="apiResult['ex:酉蓮社本IIIコレクション']">
+          <!-- <v-chip color="error">9/11 [A]酉蓮社本のURLは今後修正</v-chip> -->
+          <v-tooltip
+            v-for="(obj, index2) in apiResult['ex:酉蓮社本IIIコレクション']"
+            :key="index2"
+            bottom
+          >
+            <template #activator="{ on, attrs }">
+              <!-- {{ obj['ex:url'] }} -->
+              <a
+                v-bind="attrs"
+                :href="`${obj['ex:url']
+                  .split('https://static.toyobunko-lab.jp/u-renja')
+                  .join(urenja)}`"
+                target="_blank"
+                v-on="on"
+              >
+                <img
+                  width="30px"
+                  class="mr-1"
+                  :src="baseUrl + '/img/iiif-logo.webp'"
+                />
+              </a>
+            </template>
+            <span>{{ $utils.format(obj['ex:経典名']) }}</span>
+          </v-tooltip>
+        </template>
+
+        <template v-if="apiResult['ex:底本校本URL']">
+          <v-tooltip
+            v-for="(obj, index2) in apiResult['ex:底本校本URL']"
+            :key="index2"
+            bottom
+          >
+            <template #activator="{ on, attrs }">
+              <!-- [0] -->
+              <a
+                v-bind="attrs"
+                :href="obj['ex:画像URL']"
+                target="_blank"
+                v-on="on"
+              >
+                <img
+                  width="30px"
+                  class="mr-1"
+                  :src="baseUrl + '/img/iiif-logo.webp'"
+                />
+              </a>
+            </template>
+            <span
+              >{{ $utils.format(obj['ex:資料群名']) }}<br />{{
+                $utils.format(obj['ex:經典名稱'])
+              }}</span
+            >
+          </v-tooltip>
+        </template>
+
+        <!--
+
+                <template v-if="apiResult.images">
+                  <IIIFIcon
+                    v-for="(item2, key2) in apiResult.images"
+                    :key="key2"
+                    :item="item2"
+                  ></IIIFIcon>
+                </template>
+
+                -->
+      </td>
+      <td class="pl-1 text-left">
+        {{ $utils.format(apiResult['ex:脚注'][0]['ex:備考']) }}
+      </td>
+    </tr>
+  </table>
+</template>
+
+<script lang="ts">
+import { Vue, Prop, Component } from 'nuxt-property-decorator'
+import PagesItemSat from '~/components/pages/item/Sat.vue'
+import PagesSearchKeiten from '~/components/pages/search/Keiten.vue'
+import CommonIconShowHide from '~/components/common/icon/ShowHide.vue'
+
+@Component({
+  components: {
+    PagesItemSat,
+    PagesSearchKeiten,
+    CommonIconShowHide,
+  },
+})
+export default class KandoMokurokuSyozai extends Vue {
+  baseUrl = process.env.BASE_URL
+
+  colsKihon: string[] = [
+    '経典番号',
+    '枝番',
+    '経典名',
+    '収録巻次',
+    '部門',
+    '配本',
+    '出版年月日',
+    'SAT大蔵経DB',
+  ]
+
+  get kihon() {
+    return this.apiResult.data.relationships[
+      '基本情報 + SAT頭出し用 + 勘同目録IIIFコレクション'
+    ].data
+  }
+
+  rowAdd: string[] = ['❸麗', '❸宋', '❸元', '❸明', '❸縮', '❸卍', '❸Nj']
+
+  rowLast: string[] = ['規範碼', '高麗藏經號', '作訳者1', '作訳者2', '作訳者3']
+
+  rowHide: string[] = [
+    '❶名称〔日〕',
+    '❶名称〔支〕',
+    '❶名称〔梵〕',
+    '❶名称〔巴〕',
+    '❶名称〔西藏名〕',
+    '❶名称：異名略名',
+  ]
+
+  isSyomei: boolean = false
+
+  @Prop({ required: true })
+  apiResult!: any
+}
+</script>
--- pages/item/_id.vue
+++ pages/item/_id.vue
@@ -6,339 +6,22 @@
       </h2>
       <PagesItemKihon :api-result="apiResult"></PagesItemKihon>
 
-      <v-row class="text-center">
-        <v-col sm="12">
-          <table
-            border="1"
-            style="border-collapse: collapse"
-            width="100%"
-            class="my-2"
-          >
-            <tr>
-              <th colspan="4">
-                <h2>{{ '勘同目録' }}</h2>
-              </th>
-              <th bgcolor="#CFD8DC" rowspan="60" width="2%"></th>
-              <th colspan="4">
-                <h2>{{ '脚注' }}</h2>
-              </th>
-            </tr>
-            <tr>
-              <th width="10%">{{ '底本/校本' }}</th>
-              <th width="10%">{{ '❹' }}</th>
-              <th width="12%">{{ '❼' }}</th>
-              <th width="12%">{{ '❼備考' }}</th>
-              <th width="10%">{{ '底本/校本' }}</th>
-              <th width="10%">{{ '新添部分' }}</th>
-              <th width="12%">{{ 'テキスト' }}</th>
-              <th width="12%">{{ '備考' }}</th>
-            </tr>
-            <tr class="text-center">
-              <td>
-                {{ $utils.format(apiResult['ex:勘同目録'][0]['ex:底本/校本']) }}
-              </td>
-              <td class="pl-1 text-left">
-                {{ $utils.format(apiResult['ex:勘同目録'][0]['ex:❹']) }}
-              </td>
-              <td class="pl-1 text-left">
-                {{ $utils.format(apiResult['ex:勘同目録'][0]['ex:❼']) }}
-              </td>
-              <td class="pl-1 text-left">
-                {{ $utils.format(apiResult['ex:勘同目録'][0]['ex:❼備考']) }}
-              </td>
-              <td>
-                {{ $utils.format(apiResult['ex:脚注'][0]['ex:底本/校本']) }}
-              </td>
-              <td class="pl-1 text-left">
-                {{ $utils.format(apiResult['ex:脚注'][0]['ex:新添']) }}
-              </td>
-              <td class="pl-1 text-left">
-                <template
-                  v-if="apiResult['ex:酉目'] && apiResult['ex:酉目'].length > 0"
-                >
-                  <a
-                    :href="apiResult['ex:酉目'][0]['ex:url'][0]"
-                    target="_blank"
-                    >{{
-                      $utils.format(apiResult['ex:脚注'][0]['ex:テキスト'])
-                    }}
-                    <v-icon color="primary" small>mdi-exit-to-app</v-icon></a
-                  >
-                </template>
-                <template v-else>
-                  {{ $utils.format(apiResult['ex:脚注'][0]['ex:テキスト']) }}
-                </template>
+      <PagesItemText :api-result="apiResult" class="my-5"></PagesItemText>
 
-                <template v-if="apiResult['ex:酉蓮社本IIIコレクション']">
-                  <!-- <v-chip color="error">9/11 [A]酉蓮社本のURLは今後修正</v-chip> -->
-                  <v-tooltip
-                    v-for="(obj, index2) in apiResult[
-                      'ex:酉蓮社本IIIコレクション'
-                    ]"
-                    :key="index2"
-                    bottom
-                  >
-                    <template #activator="{ on, attrs }">
-                      <!-- {{ obj['ex:url'] }} -->
-                      <a
-                        v-bind="attrs"
-                        :href="`${obj['ex:url']
-                          .split('https://static.toyobunko-lab.jp/u-renja')
-                          .join(urenja)}`"
-                        target="_blank"
-                        v-on="on"
-                      >
-                        <img
-                          width="30px"
-                          class="mr-1"
-                          :src="baseUrl + '/img/iiif-logo.webp'"
-                        />
-                      </a>
-                    </template>
-                    <span>{{ $utils.format(obj['ex:経典名']) }}</span>
-                  </v-tooltip>
-                </template>
+      <v-card flat>
+        <v-tabs v-model="tab" background-color="primary" dark class="mb-4">
+          <v-tab>
+            {{ '勘同目録/脚注' }}
+          </v-tab>
+        </v-tabs>
 
-                <template v-if="apiResult['ex:底本校本URL']">
-                  <v-tooltip
-                    v-for="(obj, index2) in apiResult['ex:底本校本URL']"
-                    :key="index2"
-                    bottom
-                  >
-                    <template #activator="{ on, attrs }">
-                      <!-- [0] -->
-                      <a
-                        v-bind="attrs"
-                        :href="obj['ex:画像URL']"
-                        target="_blank"
-                        v-on="on"
-                      >
-                        <img
-                          width="30px"
-                          class="mr-1"
-                          :src="baseUrl + '/img/iiif-logo.webp'"
-                        />
-                      </a>
-                    </template>
-                    <span
-                      >{{ $utils.format(obj['ex:資料群名']) }}<br />{{
-                        $utils.format(obj['ex:經典名稱'])
-                      }}</span
-                    >
-                  </v-tooltip>
-                </template>
+        <v-tabs-items v-model="tab">
+          <v-tab-item>
+            <PagesItemNote :api-result="apiResult"></PagesItemNote>
+          </v-tab-item>
+        </v-tabs-items>
+      </v-card>
 
-                <!--
-
-                <template v-if="apiResult.images">
-                  <IIIFIcon
-                    v-for="(item2, key2) in apiResult.images"
-                    :key="key2"
-                    :item="item2"
-                  ></IIIFIcon>
-                </template>
-
-                -->
-              </td>
-              <td class="pl-1 text-left">
-                {{ $utils.format(apiResult['ex:脚注'][0]['ex:備考']) }}
-              </td>
-            </tr>
-            <tr>
-              <th colspan="4">
-                <h3>{{ '勘同目録詳細情報' }}</h3>
-              </th>
-              <th colspan="4">
-                <h3>{{ '脚注詳細情報' }}</h3>
-              </th>
-            </tr>
-            <template v-for="n of 5">
-              <template v-if="n == 1">
-                <tr :key="'r1_' + n">
-                  <td :rowspan="kFields.length + 3">
-                    {{ 'テキスト' + n }}<br />{{ '（勘同目録）' }}
-                  </td>
-                  <template
-                    v-if="apiResult['ex:勘同目録IIIFコレクション'].length > 0"
-                  >
-                    <td>勘同目録所在</td>
-                    <td colspan="2">
-                      <PagesItemKandoMokurokuSyozai
-                        :url="
-                          apiResult['ex:勘同目録IIIFコレクション'][0][
-                            'ex:url'
-                          ][0]
-                        "
-                        :pos="
-                          apiResult['ex:勘同目録IIIFコレクション'][0][
-                            'ex:pos'
-                          ][0]
-                        "
-                        :page-start="
-                          apiResult['ex:勘同目録IIIFコレクション'][0][
-                            'ex:ページ'
-                          ]
-                        "
-                        :page-start-dan="
-                          apiResult['ex:勘同目録IIIFコレクション'][0]['ex:段']
-                        "
-                        :page-end="
-                          apiResult['ex:勘同目録IIIFコレクション'][0][
-                            'ex:toページ'
-                          ]
-                        "
-                        :page-end-dan="
-                          apiResult['ex:勘同目録IIIFコレクション'][0]['ex:to段']
-                        "
-                      />
-                    </td>
-                  </template>
-                  <template v-else>
-                    <td bgcolor="#B0BEC5"></td>
-                    <td bgcolor="#B0BEC5" colspan="2"></td>
-                  </template>
-                  <td :rowspan="kFields.length + 3">
-                    {{ 'テキスト' + n }}<br />{{ '（脚注）' }}
-                  </td>
-                  <td>底本推定</td>
-                  <td class="pl-1 text-left" colspan="2">
-                    {{
-                      $utils.format(
-                        apiResult['ex:脚注'][0]['ex:texts'][0]['ex:底本推定']
-                      )
-                    }}
-                  </td>
-                </tr>
-                <tr :key="'r10_' + n">
-                  <td bgcolor="#B0BEC5"></td>
-                  <td bgcolor="#B0BEC5" colspan="2"></td>
-                  <td>略号使用</td>
-                  <td class="pl-1 text-left" colspan="2">
-                    {{
-                      $utils.format(
-                        apiResult['ex:脚注'][0]['ex:texts'][0]['ex:略号の使用']
-                      )
-                    }}
-                  </td>
-                </tr>
-                <tr :key="'r11_' + n">
-                  <td bgcolor="#B0BEC5"></td>
-                  <td bgcolor="#B0BEC5" colspan="2"></td>
-                  <td>略号解説</td>
-                  <td class="pl-1 text-left" colspan="2">
-                    {{
-                      $utils.format(
-                        apiResult['ex:脚注'][0]['ex:texts'][0]['ex:略号解説']
-                      )
-                    }}
-                  </td>
-                </tr>
-              </template>
-              <tr
-                v-for="(field, index) in kFields"
-                :key="'l2_' + n + index"
-                class="text-center"
-              >
-                <td v-if="index == 0 && n != 1" :rowspan="kFields.length">
-                  {{ 'テキスト' + n }}<br />{{ '（勘同目録）' }}
-                </td>
-                <td>{{ field }}</td>
-                <td class="pl-1 text-left" colspan="2">
-                  {{
-                    apiResult['ex:勘同目録'][0]['ex:texts'][n - 1]
-                      ? $utils.format(
-                          apiResult['ex:勘同目録'][0]['ex:texts'][n - 1][
-                            'ex:' + field
-                          ]
-                        )
-                      : ''
-                  }}
-                  {{
-                    field == '年' &&
-                    apiResult['ex:勘同目録'][0]['ex:texts'][n - 1] &&
-                    apiResult['ex:勘同目録'][0]['ex:texts'][n - 1]['ex:～年']
-                      ? ' - ' +
-                        $utils.format(
-                          apiResult['ex:勘同目録'][0]['ex:texts'][n - 1][
-                            'ex:～年'
-                          ]
-                        )
-                      : ''
-                  }}
-                </td>
-
-                <td
-                  v-if="index == 0 && n != 1"
-                  :rowspan="kFields.length"
-                  :bgcolor="n < 4 ? '' : '#B0BEC5'"
-                >
-                  <template v-if="n < 4">
-                    {{ 'テキスト' + n }}<br />{{ '（脚注）' }}
-                  </template>
-                </td>
-                <td :bgcolor="n < 4 ? '' : '#B0BEC5'">
-                  {{ n > 3 ? '' : field }}
-                </td>
-                <td
-                  class="pl-1 text-left"
-                  colspan="2"
-                  :bgcolor="n < 4 ? '' : '#B0BEC5'"
-                >
-                  {{
-                    apiResult['ex:脚注'][0]['ex:texts'][n - 1]
-                      ? $utils.format(
-                          apiResult['ex:脚注'][0]['ex:texts'][n - 1][
-                            'ex:' + field
-                          ]
-                        )
-                      : ''
-                  }}
-                </td>
-              </tr>
-            </template>
-
-            <template v-for="n of 2">
-              <tr
-                v-for="(field, index) in hFields"
-                :key="'l3_' + n + index"
-                class="text-center"
-              >
-                <td v-if="index == 0" :rowspan="hFields.length">
-                  {{ '所蔵者' + n }}<br />{{ '（勘同目録）' }}
-                </td>
-                <td>{{ field }}</td>
-                <td class="pl-1 text-left" colspan="2">
-                  {{
-                    apiResult['ex:勘同目録'][0]['ex:所蔵者'][n - 1]
-                      ? $utils.format(
-                          apiResult['ex:勘同目録'][0]['ex:所蔵者'][n - 1][
-                            'ex:' + field
-                          ]
-                        )
-                      : ''
-                  }}
-                </td>
-
-                <td v-if="index == 0" :rowspan="hFields.length">
-                  {{ '所蔵者' + n }}<br />{{ '（脚注）' }}
-                </td>
-                <td>{{ field }}</td>
-                <td class="pl-1 text-left" colspan="2">
-                  {{
-                    apiResult['ex:脚注'][0]['ex:所蔵者'][n - 1]
-                      ? $utils.format(
-                          apiResult['ex:脚注'][0]['ex:所蔵者'][n - 1][
-                            'ex:' + field
-                          ]
-                        )
-                      : ''
-                  }}
-                </td>
-              </tr>
-            </template>
-          </table>
-        </v-col>
-      </v-row>
       <v-sheet class="py-10 px-3 text-center">
         <v-btn depressed icon :href="uri" target="_blank">
           <v-tooltip bottom>
@@ -352,18 +35,6 @@
             <span>{{ $t('JSON-LD') }}</span>
           </v-tooltip>
         </v-btn>
-
-        <!--
-        <v-menu open-on-hover top offset-y>
-          <template v-slot:activator="{ on }">
-            <v-btn depressed icon v-on="on">
-              <v-icon>mdi-share-variant</v-icon>
-            </v-btn>
-          </template>
-
-          <ShareButtons :url="url" :title="title" />
-        </v-menu>
-        -->
 
         <ResultOption
           class="ma-1"
@@ -380,67 +51,36 @@
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
 import Content from '~/components/layouts/Content.vue'
-// import ShareButtons from '~/components/common/ShareButtons.vue'
 import ResultOption from '~/components/display/ResultOption.vue'
 import PagesItemKandoMokurokuSyozai from '~/components/pages/item/KandoMokurokuSyozai.vue'
 import PagesItemSat from '~/components/pages/item/Sat.vue'
 import PagesSearchKeiten from '~/components/pages/search/Keiten.vue'
 import PagesItemKihon from '~/components/pages/item/Kihon.vue'
+import PagesItemText from '~/components/pages/item/Text.vue'
+import PagesItemNote from '~/components/pages/item/Note.vue'
 
 @Component({
   components: {
     ResultOption,
-    // ShareButtons,
     Content,
     PagesItemKandoMokurokuSyozai,
     PagesItemSat,
     PagesSearchKeiten,
     PagesItemKihon,
+    PagesItemText,
+    PagesItemNote,
   },
 })
 export default class Search extends Vue {
   baseUrl = process.env.BASE_URL
 
-  colsKihon: string[] = [
-    '経典番号',
-    '枝番',
-    '経典名',
-    '収録巻次',
-    '部門',
-    '配本',
-    '出版年月日',
-    'SAT大蔵経DB',
-  ]
-
-  /*
-          <th>{{ '枝番' }}</th>
-          <th>{{ '経典名' }}</th>
-          <th>{{ '収録巻次' }}</th>
-          <th>{{ '部門' }}</th>
-          <th>{{ '配本' }}</th>
-          <th>{{ '出版年月日' }}</th>
-          <th>{{ 'SAT大蔵経DB' }}</th>
-          */
-
-  kFields: string[] = [
-    '標準名称',
-    '巻',
-    '国',
-    '時代',
-    '年',
-    '刊写者',
-    '刊写形態',
-    '関与者',
-    '関与形態',
-  ]
+  tab: number = 0
 
   head() {
     return {
       title: this.$t('詳細情報') + ': ' + this.title,
     }
   }
-
-  hFields: string[] = ['国', '所蔵者']
 
   apiResult: any = null
 
@@ -457,24 +97,8 @@ export default class Search extends Vue {
 
     const uri = process.env.BASE_URL + '/data/' + itemId + '.json'
 
-    /*
-
-    const apiResult = await context.$axios
-      .get(uri)
-      .then((response: any) => {
-        const apiResult = response.data
-        return apiResult
-      })
-      .catch((error: any) => {
-        // eslint-disable-next-line
-        console.error(error)
-      })
-    */
-
     const apiResult_ = await import(`~/static/data/${itemId}.json`)
     const apiResult = apiResult_.default
-
-    // console.log({ apiResult })
 
     return {
       id: itemId,
@@ -483,20 +107,6 @@ export default class Search extends Vue {
       url,
       title: itemId,
     }
-  }
-
-  get twitterUrl() {
-    return (
-      'https://twitter.com/intent/tweet?url=' + this.url + '&text=' + this.title
-    )
-  }
-
-  get facebookUrl() {
-    return 'https://www.facebook.com/sharer/sharer.php?u=' + this.url
-  }
-
-  get pocketUrl() {
-    return 'http://getpocket.com/edit?url=' + this.url
   }
 
   get bh(): any[] {
