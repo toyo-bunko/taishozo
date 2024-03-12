4ade8dcc0ea84421d94c60608eec823b0d788085
Satoru Nakamura <na.kamura.1263@gmail.com>
2024-03-12 12:17:27 +0900

    fix: bug

--- components/search/SearchResult.vue
+++ components/search/SearchResult.vue
@@ -3,20 +3,15 @@
     <template v-if="layoutOption === 'stats'">
       <StatsSearchResult />
     </template>
-    <template v-else>
-      <TableSearchResult />
-    </template>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
 import StatsSearchResult from '~/components/search/StatsSearchResult.vue'
-import TableSearchResult from '~/components/search/TableSearchResult.vue'
 
 @Component({
   components: {
-    TableSearchResult,
     StatsSearchResult,
   },
 })
